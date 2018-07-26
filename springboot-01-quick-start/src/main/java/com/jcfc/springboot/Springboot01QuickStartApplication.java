69b90420a7d45a5c7bba09d7c917da8c42b4e4df
yangdaobin <yangdaobin@jcfc.cn>
2018-07-26 16:49:50 +0800

    first commit

---
+++ springboot-01-quick-start/src/main/java/com/jcfc/springboot/Springboot01QuickStartApplication.java
@@ -0,0 +1,12 @@
+package com.jcfc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01QuickStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01QuickStartApplication.class, args);
+	}
+}
