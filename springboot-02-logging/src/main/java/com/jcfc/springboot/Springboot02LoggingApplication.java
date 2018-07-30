2ce7b3f28ea8771d585817d730b540949d0e7be2
yangdaobin <yangdaobin@jcfc.cn>
2018-07-30 10:10:18 +0800

    logging

---
+++ springboot-02-logging/src/main/java/com/jcfc/springboot/Springboot02LoggingApplication.java
@@ -0,0 +1,12 @@
+package com.jcfc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot02LoggingApplication.class, args);
+	}
+}
