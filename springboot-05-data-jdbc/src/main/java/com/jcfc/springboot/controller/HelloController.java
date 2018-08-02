1beb5e16836be7173d1e31af1f99d1fe73aa44f1
yangdaobin <yangdaobin@jcfc.cn>
2018-08-02 10:21:02 +0800

    jdbc

---
+++ springboot-05-data-jdbc/src/main/java/com/jcfc/springboot/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.jcfc.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @GetMapping("hello")
+    public Map<String, Object> map() {
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from jdbc");
+        return list.get(0);
+    }
+
+}
