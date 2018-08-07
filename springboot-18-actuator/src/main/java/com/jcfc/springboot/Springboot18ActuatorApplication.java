f2bd85c0cec7965c658f4fb9286545853565ae0e
yangdaobin <yangdaobin@jcfc.cn>
2018-08-07 16:16:17 +0800

    actuator

---
+++ springboot-18-actuator/src/main/java/com/jcfc/springboot/Springboot18ActuatorApplication.java
@@ -0,0 +1,18 @@
+package com.jcfc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 自定义健康状态指示器
+ * 	1、编写一个指示器，实现 HealthIndicator 接口
+ * 	2、指示器的名字规范：xxxHealthIndicator
+ * 	3、加入容器中
+ */
+@SpringBootApplication
+public class Springboot18ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot18ActuatorApplication.class, args);
+	}
+}
