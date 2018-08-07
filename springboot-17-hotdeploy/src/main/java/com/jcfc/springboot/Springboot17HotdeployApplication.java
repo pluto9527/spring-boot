203c094dbf1c49b33ab2187815e3b23ec39135f1
yangdaobin <yangdaobin@jcfc.cn>
2018-08-07 15:42:44 +0800

    hot deploy

---
+++ springboot-17-hotdeploy/src/main/java/com/jcfc/springboot/Springboot17HotdeployApplication.java
@@ -0,0 +1,12 @@
+package com.jcfc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot17HotdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot17HotdeployApplication.class, args);
+	}
+}
